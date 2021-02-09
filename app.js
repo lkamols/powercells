162906627364338a7ee70cb2047f10ae7dfd1d58
Luke Kamols <lkamols4@gmail.com>
2021-02-09 14:03:29 +1000

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,21 @@
 
 const switcher = document.querySelector('.btn');
 
+function reqListener() {
+    console.log(this.responseText);
+}
+
 switcher.addEventListener('click', function() {
     
     document.getElementById("start-msg").style.visibility = "visible";
+
+    var oReq = new XMLHttpRequest();
+    oReq.addEventListener("load", reqListener);
+    oReq.open("GET", "http://localhost:8080");
+    //oReq.setRequestHeader("Upgrade-Insecure-Requests", "1");
+    //oReq.setRequestHeader("Access-Control-Allow-Origin", "*");
+    //oReq.setRequestHeader("Content-Type", "text/plain");
+    oReq.send();
 
     console.log('process started');
 })
